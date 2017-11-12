874cba585b7af6a06bbe85f6a3bb39bb4b6cff6b
peymannj <peyman.naji@gmail.com>
2017-11-12 18:32:23 +0100

    init back-end application

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const cors = require('cors');
+ const morgan = require('morgan');
+
+ const app = new express();
+
+ app.use(morgan('combined'));
+ app.use(bodyParser.json());
+ app.use(cors());
+
+
+app.get('/status', (req, res) =>{
+    res.send({
+        message:'Hello world'
+    });
+});
+
+
+app.listen(process.env.Port || 8081);
